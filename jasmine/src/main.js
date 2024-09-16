1d67f63a935d3d1cf64b13ca2b920b33efdec156
Chris Skeen <chrisskeen2006@gmail.com>
2024-09-16 09:10:33 -0500

    Finished

--- jasmine/src/main.js
+++ jasmine/src/main.js
@@ -1,15 +1,12 @@
 // Properties for make, model, color and year.
 class Vehicle {
   constructor(make, model, color, year) {
-    super(numberOfDoors)
-    super(condition)
-    super(mileage)
     this.make = make;
     this.model = model;
     this.color = color;
     this.year = year;
   }
-
+// This will print each vehicle detail into the console.
   vehicleDetails() {
     console.log(`Make: ${this.make}`)
     console.log(`Model: ${this.model}`)
@@ -17,15 +14,27 @@ class Vehicle {
     console.log(`Year: ${this.year}`)
   }
 }
-
+// Subclass "Car" adds doors, mileage, and condition.
 class Car extends Vehicle {
-  constructor(make, model, color, year, doors, mileage) {
+  constructor(make, model, color, year, doors, mileage, condition) {
     super(make, model, color, year)
     this.doors = doors;
     this.mileage = mileage;
+  }
+  vehicleDetails(){
+    console.log(`Make: ${this.make}`)
+    console.log(`Model: ${this.model}`)
+    console.log(`Color: ${this.color}`)
+    console.log(`Year: ${this.year}`)
+    console.log(`Number of Doors: ${this.doors}`)
+    console.log(`Mileage: ${this.mileage} miles`)
 
-    if (this.mileage > 10000) {
-      usedValue = 'New'
+    if (this.mileage < 10000) {
+      this.condition = 'New'
+      console.log(`Condition: ${this.condition}`)
+    } else {
+      this.condition = 'Used'
+      console.log(`Condition: ${this.condition}`)
     }
   }
 }
